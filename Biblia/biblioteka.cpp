48e2c557b6e8583d1c33649cebd436bbcb1d2b1a
scary101 <scary101@yandex.ru>
2024-06-05 12:26:22 +0300

    Добавьте файлы проекта.

---
+++ Biblia/biblioteka.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+using namespace std;
+
+typedef bool (*ContainsAllCharactersFunc)(const char*, const char*);
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    string inputString, searchChars;
+    cout << "Введите строку: ";
+    std::getline(cin, inputString);
+    cout << "Введите символы для поиска: ";
+    std::getline(cin, searchChars);
+    HINSTANCE load = LoadLibrary(TEXT("DinBiblia.dll"));
+
+    ContainsAllCharactersFunc containsAllCharacters = (ContainsAllCharactersFunc)GetProcAddress(load, "containsAllCharacters");
+
+    bool result = containsAllCharacters(inputString.c_str(), searchChars.c_str());
+
+    if (result) {
+        cout << "В строке найден символ" << endl;
+    }
+    else {
+        cout << "В строке не найдено" << endl;
+    }
+
+    FreeLibrary(load);
+
+    return 0;
+}
---
+++ DinBiblia/dllmain.cpp
@@ -0,0 +1,32 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include <string>
+using namespace std;
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+extern "C" __declspec(dllexport) bool containsAllCharacters(const char* str, const char* chars) {
+    string s(str);
+    string c(chars);
+
+    for (char ch : c) {
+        if (s.find(ch) == string::npos) {
+            return false;
+        }
+    }
+    return true;
+}
